a602a1900e3c2a85de97e5bb43ce27aac183178b
programerhasan0 <programmerhasan0@gmail.com>
2025-11-28 14:57:09 +0600

    added appointment search functionality

--- frontend/src/pages/MyAppointments.tsx
+++ frontend/src/pages/MyAppointments.tsx
@@ -2,12 +2,13 @@ import Appointment from '@components/Appointment';
 import { AppContext } from '@context/contexts';
 import { PatientAppointment } from '@definitions/utils';
 import axios from 'axios';
-import { useContext, useEffect, useState } from 'react';
+import { useContext, useEffect, useMemo, useState } from 'react';
 import { toast } from 'react-toastify';
 
 const MyAppointments: React.FC = () => {
-    const { doctors, token } = useContext(AppContext);
+    const { token } = useContext(AppContext);
     const [appointments, setAppointments] = useState<PatientAppointment[]>([]);
+    const [searchQuery, setSearchQuery] = useState<string>('');
 
     useEffect(() => {
         axios
@@ -27,13 +28,36 @@ const MyAppointments: React.FC = () => {
             });
     }, [token]);
 
+    const filteredAppointments = useMemo(() => {
+        if (!appointments) return [];
+
+        return appointments.filter((appointment) => {
+            const q = searchQuery.toLowerCase();
+            return (
+                appointment.doctor.name.toLowerCase().includes(q) ||
+                appointment.doctor.qualification.toLowerCase().includes(q) ||
+                appointment.doctor.speciality.title.toLowerCase().includes(q) ||
+                appointment.doctor.speciality.department
+                    .toLowerCase()
+                    .includes(q) ||
+                new Date(appointment.date).toLocaleDateString().includes(q)
+            );
+        });
+    }, [appointments, searchQuery]);
+
     return (
         <div>
-            <p className="pb-3 mt-12 font-medium text-zinc-700 border-b border-gray-300">
+            <p className="pb-3 mt-12 font-medium text-zinc-700">
                 My Appointments
             </p>
+            <input
+                type="text"
+                className="w-full p-2 border border-gray-200 rounded-sm my-2 outline-none focus:border-primary transition-all duration-200"
+                placeholder="Search Appointments by doctor name, speciality, department or date"
+                onChange={(e) => setSearchQuery(e.target.value)}
+            />
             <div>
-                {appointments.slice(0, 3).map((item, idx) => (
+                {filteredAppointments.map((item, idx) => (
                     <Appointment item={item} key={idx} />
                 ))}
             </div>
